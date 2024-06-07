a9eb5e7b0b209853a218924aa39573854a0f9323
Pr3pared <crkoshay@gmail.com>
2024-06-07 12:01:33 -0700

    added modal name/title functionality

--- scripts/index.js
+++ scripts/index.js
@@ -28,6 +28,7 @@ const initialCards = [
 let editButton = document.querySelector(".profile__edit-button");
 let modal = document.querySelector(".modal");
 let modalClose = document.querySelector(".modal__close");
+let modalForm = document.querySelector(".modal__form")
 
 editButton.addEventListener("click", function(){
     modal.classList.remove("modal_hidden");
@@ -36,3 +37,21 @@ editButton.addEventListener("click", function(){
 modalClose.addEventListener("click", function(){
     modal.classList.add("modal_hidden");
 })
+
+//Shows on site
+const profileName = document.querySelector(".profile__name");
+const profileTitle = document.querySelector(".profile__title");
+
+//New Submitted Names
+const submittedName = document.querySelector(".modal__form-name");
+const submittedTitle = document.querySelector(".modal__form-title");
+
+modalForm.addEventListener("submit", modalFormSubmit);
+
+
+function modalFormSubmit(event) {
+    event.preventDefault();
+    profileName.textContent = submittedName.value;
+    profileTitle.textContent = submittedTitle.value;
+    modal.classList.add("modal_hidden");
+}
